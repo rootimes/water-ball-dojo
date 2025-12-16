aefe69303d3d6c547bb6c66b9571bdf3102ae467
rootimes <rootimes8596@gmail.com>
2025-12-16 21:52:26 +0800

    feat: implement DistanceBasedMatchStrategy and HabitBasedMatchStrategy for candidate sorting based on distance and habit overlap

---
+++ two/src/main/java/matchmakingsystem/matchingSystem/matchStrategy/MatchStrategy.java
@@ -0,0 +1,8 @@
+package matchMakingSystem.matchingSystem.matchStrategy;
+
+import java.util.List;
+import matchMakingSystem.individual.Individual;
+
+public interface MatchStrategy {
+    public List<Individual> sorted(Individual individual, List<Individual> candidates);
+}
