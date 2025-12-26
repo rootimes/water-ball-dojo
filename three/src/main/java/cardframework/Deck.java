ed45c8a25fe03b1084387cb62716c44daf9c2935
rootimes <rootimes8596@gmail.com>
2025-12-26 16:24:44 +0800

    feat: add Deck and HandCard classes for card management

---
+++ three/src/main/java/cardframework/Deck.java
@@ -0,0 +1,30 @@
+package cardframework;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public abstract class Deck<TCard> {
+    
+    protected final List<TCard> cards = new ArrayList<>();
+
+    public Deck(List<TCard> cards) {
+        this.cards.addAll(cards);
+    }
+    
+    public void shuffle() {
+        Collections.shuffle(cards);
+    }
+
+    public TCard draw() {
+        if (this.size() == 0) {
+            throw new IllegalStateException("No cards left in the deck to draw.");
+        }
+
+        return cards.remove(cards.size() - 1);
+    }
+
+    public int size() {
+        return cards.size();
+    }
+}
---
+++ three/src/main/java/cardframework/HandCard.java
@@ -0,0 +1,9 @@
+package cardframework;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class HandCard<TCard> {
+    
+    protected List<TCard> cards = new ArrayList<>();
+}
