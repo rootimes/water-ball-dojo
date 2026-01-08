4bef3170380d95efe4eb679024236500c71a789c
rootimes <rootimes8596@gmail.com>
2026-01-08 18:12:04 +0800

    feat: implement BigTwo game with start and end methods

---
+++ four/src/main/java/bigTwo/Main.java
@@ -0,0 +1,13 @@
+package bigTwo;
+
+import bigTwo.game.BigTwo;
+
+public class Main {
+    public static void main(String[] args) {
+        BigTwo game = new BigTwo();
+
+        game.start();
+
+        game.end();
+    }
+}
---
+++ four/src/main/java/bigTwo/game/BigTwo.java
@@ -0,0 +1,12 @@
+package bigTwo.game;
+
+public class BigTwo {
+    
+    public void start() {
+        System.out.println("Big2遊戲開始！");
+    }
+
+    public void end() {
+        System.out.println("Big2遊戲結束！");
+    }
+}
