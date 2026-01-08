f15795b1ff43fd7b45df5de39c161d7f66a91cbf
rootimes <rootimes8596@gmail.com>
2026-01-08 19:04:06 +0800

    feat: add Straight class for BigTwo game with comparison and validation logic

---
+++ four/src/main/java/bigTwo/CardPattern/Straight.java
@@ -0,0 +1,53 @@
+package bigTwo.CardPattern;
+
+import java.util.List;
+import bigTwo.Card.Card;
+
+public class Straight extends CardPattern<Straight> {
+    private int size = 5;
+
+    public Straight(List<Card> cards) {
+        this.cards = cards;
+    }
+
+    @Override
+    public int compareTo(Straight other) {
+        Card thisHighest = this.getHighestCard();
+        Card otherHighest = other.getHighestCard();
+
+        int rankCmp = Integer.compare(thisHighest.getRankValue(), otherHighest.getRankValue());
+        if (rankCmp != 0) {
+            return rankCmp;
+        }
+
+        return Integer.compare(thisHighest.getSuitValue(), otherHighest.getSuitValue());
+    }
+
+    @Override
+    public boolean validate(List<Card> cards) {
+        if (isValidSize(cards)) {
+            for (int i = 0; i < size - 1; i++) {
+                Card a = cards.get(i);
+                Card b = cards.get(i + 1);
+                int currentRank = a.getRankValue();
+                int nextRank = b.getRankValue();
+                if (nextRank != currentRank + 1) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private Card getHighestCard() {
+        Card highest = getCard(0);
+        for (Card card : cards) {
+            if (card.getRankValue() > highest.getRankValue() ||
+                    (card.getRankValue() == highest.getRankValue() &&
+                            card.getSuitValue() > highest.getSuitValue())) {
+                highest = card;
+            }
+        }
+        return highest;
+    }
+}
