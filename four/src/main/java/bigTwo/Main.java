ba756d7afc3ee48b920b847ab5d98147a0a03311
rootimes <rootimes8596@gmail.com>
2026-01-08 23:15:05 +0800

    style: format code for consistency and readability in Main class

--- four/src/main/java/bigTwo/Main.java
+++ four/src/main/java/bigTwo/Main.java
@@ -3,11 +3,11 @@ package bigTwo;
 import bigTwo.game.BigTwo;
 
 public class Main {
-    public static void main(String[] args) {
-        BigTwo game = new BigTwo();
+	public static void main(String[] args) {
+		BigTwo game = new BigTwo();
 
-        game.start();
+		game.start();
 
-        game.end();
-    }
+		game.end();
+	}
 }
